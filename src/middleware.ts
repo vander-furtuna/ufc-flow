639238a3be54eee2efadbd8ea3e902112bab238b
Vanderlei Furtuna <furtunavanderlei@gmail.com>
2024-10-08 21:53:13 -0300

    refactor: change middleware to allow banner route

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,10 @@ import { getUrl } from './lib/get-url'
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname
 
+  if (pathname.includes('/banner.png')) {
+    return
+  }
+
   if (!pathname.includes('/engenharia-de-computacao-ufc-sobral/2006-2')) {
     return NextResponse.redirect(
       new URL(getUrl('/engenharia-de-computacao-ufc-sobral/2006-2')),
