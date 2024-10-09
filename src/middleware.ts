8ad5ddba4d304c9c1c1ada4146d008539ea98ce9
Vanderlei Furtuna <furtunavanderlei@gmail.com>
2024-10-08 22:22:07 -0300

    fix: middleware route matches

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,5 +17,7 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|icon.svg).*)'],
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico|icon.svg|banner.png).*)',
+  ],
 }
