0d8813149196a0ecdfb2a5f0812955078ef6598f
Vanderlei Furtuna <furtunavanderlei@gmail.com>
2025-05-14 21:16:34 -0300

    refactor: vander dev logo

---
+++ src/constants/logo-colors.ts
@@ -0,0 +1,14 @@
+export type LogoColor = {
+  from: string
+  to: string
+}
+
+export const LOGO_COLORS: LogoColor[] = [
+  { from: '#FF9F5D', to: '#F97316' },
+  { from: '#A874FF', to: '#7C3AED' },
+  { from: '#5DEAFF', to: '#22D3EE' },
+  { from: '#FFCD4D', to: '#FFB800' },
+  { from: '#85FF4C', to: '#5BE41B' },
+  { from: '#FFB800', to: '#5BE41B' },
+  { from: '#22D3EE', to: '#FFB800' },
+]
