cb4ea8ae1b7dd64434bace65282f00be6347d38e
Vanderlei Furtuna <furtunavanderlei@gmail.com>
2025-06-07 13:00:27 -0300

    style: add pride month logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ export function Header() {
   return (
     <header className="flex w-full items-start justify-between">
       <div>
-        <Logo className="h-12 w-auto" />
+        <Logo className="h-14 w-auto" />
       </div>
       <div className="flex items-center gap-1">
         <SubtitlePopup />
