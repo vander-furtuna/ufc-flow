c1741dad6917c8103a298b8b51099f940d84bd8d
Vanderlei Furtuna <furtunavanderlei@gmail.com>
2025-08-04 22:36:19 -0300

    fix: remove unused SubtitlePopup component from Header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,5 @@
 import { Logo } from './logo'
 import { ModeToggle } from './theme-toggle'
-import { SubtitlePopup } from './ui/subtitle-popup'
 
 export function Header() {
   return (
@@ -9,7 +8,6 @@ export function Header() {
         <Logo className="h-14 w-auto" />
       </div>
       <div className="flex items-center gap-1">
-        <SubtitlePopup />
         <ModeToggle />
       </div>
     </header>
