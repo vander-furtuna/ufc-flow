a521b048c92b23007dd320582d2242186f66037d
Vanderlei Furtuna <furtunavanderlei@gmail.com>
2025-12-12 09:17:52 -0300

    feat: add NotFound component for handling 404 errors

--- src/app/[courseSlug]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Header } from '@/components/header'
-
-export default function Course() {
-  return (
-    <main className="min-h-dvh w-full pt-12 pb-24">
-      <Header />
-    </main>
-  )
-}
