0ab6df308a29ef45a2a9cc2be3d9de5f5d265a4a
Vanderlei Furtuna <furtunavanderlei@gmail.com>
2025-12-12 10:00:34 -0300

    refactor: remove console log and update fetchScheduleData parameters

--- src/app/[courseSlug]/[curriculumSlug]/page.tsx
+++ src/app/[courseSlug]/[curriculumSlug]/page.tsx
@@ -11,8 +11,6 @@ type CurriculumProps = {
 export default async function Curriculum({ params }: CurriculumProps) {
   const loadedParams = await params
 
-  console.log('Loaded Params:', loadedParams)
-
   return (
     <main className="flex h-full min-h-dvh w-full items-start justify-center gap-6 px-6 md:gap-12">
       <CurriculumSection params={loadedParams} />
