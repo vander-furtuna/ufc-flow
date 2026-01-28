d0739dd08780476052750e36fd513f71c0474f20
Vanderlei Furtuna <furtunavanderlei@gmail.com>
2026-01-28 13:54:06 -0300

    fix: update skeleton component to display six elements instead of five

--- src/app/[courseSlug]/[curriculumSlug]/components/subject-diagram-skeleton.tsx
+++ src/app/[courseSlug]/[curriculumSlug]/components/subject-diagram-skeleton.tsx
@@ -18,7 +18,7 @@ export function SubjectDiagramSkeleton() {
           <SemesterTitle>{period}º PERÍODO</SemesterTitle>
           <div className="grid w-full grid-flow-dense auto-rows-[100px] grid-cols-[repeat(auto-fit,minmax(9rem,1fr))] justify-items-center gap-4">
             <AnimatePresence>
-              {Array.from({ length: 5 }, (_, i) => i + 1).map((element) => {
+              {Array.from({ length: 6 }, (_, i) => i + 1).map((element) => {
                 return (
                   <div
                     className="font-clash ring-border center h-24 w-38 animate-pulse rounded-lg bg-slate-100 ring-1 dark:bg-slate-900"
